48cc0df410de7c8bb19d1653eff54945f388c95d
fonsi <alfonso.alfurtx@gmail.com>
2022-01-19 10:58:55 +0100

    added: add highlight to common groups

---
+++ colors.lua
@@ -0,0 +1,45 @@
+vim.cmd([[
+  hi Normal             guibg=#161616 guifg=#cdaa7d
+  hi Keyword            guifg=#cd950c
+  hi CursorColumn       gui=NONE guibg=NONE guifg=NONE
+  hi CursorLine         guibg=#191970
+  hi Comment            guifg=#7f7f7f
+  hi Conceal            guifg=#7f7f7f
+  hi Constant           guifg=#6b8e23
+  hi Cursor             guibg=#40ff40 guifg=#161616
+  hi Delimiter          gui=bold guifg=#dab98f
+  hi DiffAdd            guifg=#dab98f
+  hi DiffChange         guifg=#dab98f
+  hi DiffDelete         gui=italic guifg=Red
+  hi DiffText           guifg=#dab98f
+  hi Directory          guifg=#6b8e23
+  hi ErrorMsg           guifg=#d42a2a
+  hi WarningMsg         guifg=#d4d42a
+  hi EndOfBuffer        guibg=#161616 guifg=#161616
+  hi NonText            gui=NONE guibg=NONE guifg=NONE
+  hi Whitespace         gui=NONE guibg=NONE guifg=NONE
+  hi Folded             gui=NONE guibg=NONE guifg=NONE
+  hi FoldColumn         gui=NONE guibg=NONE guifg=NONE
+  hi Function           guifg=#cdaa7d
+  hi Identifier         guifg=#cdaa7d
+  hi Ignore             gui=NONE guibg=NONE guifg=NONE
+  hi Include            guifg=#dab98f
+  hi IncSearch          gui=bold guifg=#cd950c
+  hi LineNr             gui=NONE guibg=NONE guifg=NONE
+  hi MatchParen         guifg=#dab98f
+  hi Pmenu              guibg=#161616 guifg=#cdaa7d
+  hi PmenuSbar          gui=NONE guibg=NONE guifg=NONE
+  hi PmenuSel           guibg=#cdaa7d guifg=#161616
+  hi PmenuThumb         gui=NONE guibg=NONE guifg=NONE
+  hi QuickFixLine       gui=NONE guibg=NONE guifg=NONE
+  hi Search             gui=NONE guibg=NONE guifg=NONE
+  hi SignColumn         gui=NONE guibg=NONE guifg=NONE
+  hi StorageClass       guifg=#cd950c
+  hi String             guifg=#6b8e23
+  hi Structure          guifg=#cd950c
+  hi Todo               gui={bold, underline} guifg=Red
+  hi Type               guifg=#cd950c
+  hi Underlined         gui=italic guifg=#6b8e23
+  hi Visual             guibg=#191970
+  hi VisualNOS          guibg=#191970
+]])
